88bd9e5c0cd1d5d7c41245900e7b6bc671919fdf
Timothy7310 <penlist127@gmail.com>
2023-06-28 10:22:28 +0300

    feat: add ThreadList & ThreadItem components

---
+++ vue-school.io/vue-school-forum/src/components/ThreadList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-full">
+    <div class="thread-list">
+      <h2 class="list-title">Threads</h2>
+      <thread-item
+        v-for="thread in threads"
+        :thread="thread"
+        :key="thread.id"
+      ></thread-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import ThreadItem from "@/components/ThreadItem.vue";
+import sourceData from "@/data.json";
+export default {
+  components: {
+    ThreadItem,
+  },
+  props: {
+    threads: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((p) => p.id === postId);
+    },
+
+    userById(userId) {
+      return this.users.find((p) => p.id === userId);
+    },
+  },
+};
+</script>
+
+<style></style>
