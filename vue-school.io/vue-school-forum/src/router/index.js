bbd684d38687d091a33fb6b18aefb19ac211c5cc
Timothy7310 <penlist127@gmail.com>
2023-06-28 12:26:20 +0300

    feat: add PageNotFound

--- vue-school.io/vue-school-forum/src/router/index.js
+++ vue-school.io/vue-school-forum/src/router/index.js
@@ -1,5 +1,7 @@
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
+import PageNotFound from "@/components/PageNotFound";
+import sourceData from "@/data.json";
 
 import {
   createRouter,
@@ -18,6 +20,27 @@ const routes = [
     name: "ThreadShow",
     component: PageThreadShow,
     props: true,
+    beforeEnter: (to, from, next) => {
+      const threadExists = !!sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+
+      if (threadExists) {
+        return next();
+      } else {
+        next({
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFound,
   },
 ];
 
