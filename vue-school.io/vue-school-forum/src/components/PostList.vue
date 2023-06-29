7076a684c9818192bcbfa53314036c92f9f96067
Timothy7310 <penlist127@gmail.com>
2023-06-29 16:23:57 +0300

    feat: add global component AppDate

--- vue-school.io/vue-school-forum/src/components/PostList.vue
+++ vue-school.io/vue-school-forum/src/components/PostList.vue
@@ -32,11 +32,7 @@
 
 <script>
 import sourceData from "@/data.json";
-import AppDate from "@/components/AppDate.vue";
 export default {
-  components: {
-    AppDate,
-  },
   props: {
     posts: {
       type: Array,
--- vue-school.io/vue-school-forum/src/main.js
+++ vue-school.io/vue-school-forum/src/main.js
@@ -2,4 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+const requireComponent = require.context(
+  "./components",
+  true,
+  /App[A-Z]\w+\.(vue|js)$/
+);
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName);
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig;
+  const baseComponentName =
+    baseComponentConfig.name ||
+    fileName.replace(/^.+\//, "").replace(/\.\w+$/, "");
+  app.component(baseComponentName, baseComponentConfig);
+});
+
+app.use(router).mount("#app");
