7685acbd3a81096ab5c584a36b96535774b9308a
Timothy7310 <penlist127@gmail.com>
2023-06-29 17:27:22 +0300

    feat: add CategoryList component

---
+++ vue-school.io/vue-school-forum/src/components/CategoryList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="forum-list" v-for="category in categories" :key="category.id">
+    <h2 class="list-title">
+      <a href="#">{{ category.name }}</a>
+    </h2>
+    <forum-list :forumIDs="category.forums" />
+  </div>
+</template>
+
+<script>
+import ForumList from "@/components/ForumList.vue";
+export default {
+  components: {
+    ForumList,
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue-school.io/vue-school-forum/src/components/ForumList.vue
+++ vue-school.io/vue-school-forum/src/components/ForumList.vue
@@ -1,41 +1,44 @@
 <template>
-  <div class="forum-list">
-    <h2 class="list-title">
-      <a href="#">Forums</a>
-    </h2>
-    <div class="forum-listing" v-for="forum in forums" :key="forum.id">
-      <div class="forum-details">
-        <router-link
-          class="text-xlarge"
-          :to="{ name: 'Forum', params: { id: forum.id } }"
-        >
-          {{ forum.name }}
-        </router-link>
-        <p>{{ forum.description }}</p>
-      </div>
-
-      <div class="threads-count">
-        <p>
-          <span v-if="forum.threads" class="count">{{
-            forum.threads.length
-          }}</span>
-          <span v-else class="count">0</span>
-          {{ forum.threads?.length > 1 ? "threads" : "thread" }}
-        </p>
-      </div>
-
-      <div class="last-thread"></div>
+  <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+    <div class="forum-details">
+      <router-link
+        class="text-xlarge"
+        :to="{ name: 'Forum', params: { id: forum.id } }"
+      >
+        {{ forum.name }}
+      </router-link>
+      <p>{{ forum.description }}</p>
     </div>
+
+    <div class="threads-count">
+      <p>
+        <span v-if="forum.threads" class="count">{{
+          forum.threads.length
+        }}</span>
+        <span v-else class="count">0</span>
+        {{ forum.threads?.length > 1 ? "threads" : "thread" }}
+      </p>
+    </div>
+
+    <div class="last-thread"></div>
   </div>
 </template>
 
 <script>
+import sourceData from "@/data.json";
 export default {
   props: {
-    forums: {
+    forumIDs: {
       type: Array,
       required: true,
     },
+  },
+  data() {
+    return {
+      forums: this.forumIDs.map((forumId) =>
+        sourceData.forums.find((forum) => forum.id === forumId)
+      ),
+    };
   },
 };
 </script>
