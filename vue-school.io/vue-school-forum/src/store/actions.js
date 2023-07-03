bae70794e29b7c95830bfe9421c65e064f7b59fe
Timothy7310 <penlist127@gmail.com>
2023-07-03 22:47:23 +0300

    feat: save posts to firebase

--- vue-school.io/vue-school-forum/src/store/actions.js
+++ vue-school.io/vue-school-forum/src/store/actions.js
@@ -2,13 +2,26 @@ import firebase from "firebase";
 import { findById } from "@/helpers";
 
 export default {
-  createPost({ commit, state }, post) {
-    post.id = "qqqq" + Math.random();
+  async createPost({ commit, state }, post) {
     post.userId = state.authId;
     post.publishedAt = Math.floor(Date.now() / 1000);
-    commit("setItem", { resource: "posts", item: post });
+
+    const batch = firebase.firestore().batch();
+    const postRef = firebase.firestore().collection("posts").doc();
+    const threadRef = firebase
+      .firestore()
+      .collection("threads")
+      .doc(post.threadId);
+    batch.set(postRef, post);
+    batch.update(threadRef, {
+      posts: firebase.firestore.FieldValue.arrayUnion(postRef.id),
+      contributors: firebase.firestore.FieldValue.arrayUnion(state.authId),
+    });
+    await batch.commit();
+
+    commit("setItem", { resource: "posts", item: { ...post, id: postRef.id } });
     commit("appendPostToThread", {
-      childId: post.id,
+      childId: postRef.id,
       parentId: post.threadId,
     });
     commit("appendContributorToThread", {
