975701d5c3a56f54c3bd2470f48a24c1d9747977
Timothy7310 <penlist127@gmail.com>
2023-07-03 17:18:17 +0300

    feat: add info from firebase

--- vue-school.io/vue-school-forum/src/helpers/index.js
+++ vue-school.io/vue-school-forum/src/helpers/index.js
@@ -1,4 +1,8 @@
 export const findById = (resources, id) => {
+  if (!resources) {
+    return null;
+  }
+
   return resources.find((r) => r.id === id);
 };
 
--- vue-school.io/vue-school-forum/src/store/index.js
+++ vue-school.io/vue-school-forum/src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from "vuex";
-import sourceData from "@/data.json";
 import { findById, upsert } from "@/helpers";
 
 export default createStore({
   state: {
-    ...sourceData,
+    categories: [],
+    forums: [],
+    threads: [],
+    posts: [],
+    users: [],
     authId: "L664y3qZSubDbT1R6npC0EEybJ73",
   },
   getters: {
@@ -119,9 +122,8 @@ export default createStore({
       upsert(state.threads, thread);
     },
 
-    setUser(state, { user, userId }) {
-      const userIndex = state.users.findIndex((user) => user.id === userId);
-      state.users[userIndex] = user;
+    setUser(state, { user }) {
+      upsert(state.users, user);
     },
 
     appendPostToThread: makeAppendChildToParentMutation({
