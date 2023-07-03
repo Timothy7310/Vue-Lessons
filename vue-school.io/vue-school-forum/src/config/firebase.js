0efaa744819e2b0009e5a39f7ad4621fa1ec849f
Timothy7310 <penlist127@gmail.com>
2023-07-03 15:59:35 +0300

    feat: add firebase

---
+++ vue-school.io/vue-school-forum/src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyDM1uRq7xMNnKwlDhgR15yauVnspbMg2CU",
+  authDomain: "vue-school-forum-dfbf6.firebaseapp.com",
+  projectId: "vue-school-forum-dfbf6",
+  storageBucket: "vue-school-forum-dfbf6.appspot.com",
+  messagingSenderId: "676808967230",
+  appId: "1:676808967230:web:dd1caa4d79fa52fccdfd6f",
+};
